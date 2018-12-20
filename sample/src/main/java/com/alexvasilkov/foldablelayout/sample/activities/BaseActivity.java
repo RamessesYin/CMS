d0a556101e1379360c8154d63750225d630678d9
unknown <user@DESKTOP-D0GOJTH.sjtu.edu.cn>
2018-12-20 15:21:34 +0800

    UI6

--- sample/src/main/java/com/alexvasilkov/foldablelayout/sample/activities/BaseActivity.java
+++ sample/src/main/java/com/alexvasilkov/foldablelayout/sample/activities/BaseActivity.java
@@ -110,18 +110,6 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     private void scanAddress() {
-//        new IntentIntegrator(getActivity())
-//                // 扫码的类型,条形码或者二维码
-//                .setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES)
-//                //这个参数可以设置也可以不设置，设置后使用自定义的二维码扫描框，不设置使用默认的
-//                .setPrompt("将二维码放到框内")
-//                //是否在扫描完成后有提示音
-//                .setBeepEnabled(true)
-//                //扫完码之后生成二维码的图片
-//                .setBarcodeImageEnabled(true)
-//                //初始化扫描
-//                .initiateScan();
-
         Intent i = new Intent(this, QrCodeActivity.class);
         startActivityForResult(i, REQUEST_CODE_QR_SCAN);
     }
